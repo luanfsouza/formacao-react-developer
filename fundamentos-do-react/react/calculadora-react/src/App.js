39a84fff7d2055f6585a115100b658e83bd5c2ac
luanfsouza <luanferreira7777@gmail.com>
2024-07-21 18:01:42 -0300

    calculadora react ajuste

--- fundamentos-do-react/react/calculadora-react/src/App.js
+++ fundamentos-do-react/react/calculadora-react/src/App.js
@@ -5,30 +5,79 @@ import { useState } from "react";
 
 function App() {
   const [currentNumber, setCurrentNumber] = useState("0")
-  let firstNumber = "0";
-  
-  const handleClear = () => setCurrentNumber("0");
+  const [firstNumber, setFirstNumber] = useState("0")
+  const [operation, setOperation] = useState("")
+
+  const handleClear = () => {
+    setCurrentNumber("0")
+    setFirstNumber("0")
+    setOperation("")
+  };
 
   const handleAddNumber = (number) => {
     setCurrentNumber(valorPrevio => `${valorPrevio === "0" ? "" : valorPrevio}${number}`)
-    console.log("currentNumber: ", Number(currentNumber))
-    console.log("firstNumber: ", Number(firstNumber))
   }
 
   const handleSumNumbers = () => {
     if (firstNumber === "0") {
-      firstNumber = currentNumber;
-      
-      console.log("firstNumber: ", Number(firstNumber))
+      setFirstNumber(currentNumber);
+      setCurrentNumber("0")
+      setOperation("+");
+    } else {
+      const num = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(num))
+    }
+  }
+  const handleMultiplicationNumbers = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(currentNumber);
+      setCurrentNumber("0")
+      setOperation("x");
+    } else {
+      const num = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(num))
+    }
+  }
+  const handleDivisionNumbers = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(currentNumber);
+      setCurrentNumber("0")
+      setOperation("/");
+    } else {
+      const num = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(num))
+    }
+  }
+  const handleRemNumbers = () => {
+    if (firstNumber === "0") {
+      setFirstNumber(currentNumber);
+      setCurrentNumber("0")
+      setOperation("-");
+    } else {
+      const num = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(num))
     }
   }
 
-  const igual = () => {
-    
-      const num = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(valorPrevio => String(num))
-      //firstNumber = "0";
-    
+  const handleEquals = () => {
+    if (firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
+      switch (operation) {
+        case "+":
+          handleSumNumbers()
+          break;
+        case "-":
+          handleRemNumbers()
+          break;
+        case "x":
+          handleMultiplicationNumbers()
+          break;
+        case "/":
+          handleDivisionNumbers()
+          break;
+        default:
+          break;
+      }
+    }
   }
 
   return (
@@ -36,16 +85,17 @@ function App() {
       <Content>
         <Input value={currentNumber} />
         <Rom>
-          <Button label="x" />
-          <Button label="/" />
-          <Button label="C" onClick={() => handleClear()} />
-          <Button label="X" />
+          <Button label="x" onClick={handleMultiplicationNumbers}/>
+          <Button label="/" onClick={handleDivisionNumbers}/>
+          <Button label="." />
+          <Button label="c" onClick={() => handleClear()} />
+          
         </Rom>
         <Rom>
           <Button label="7" onClick={() => handleAddNumber("7")} />
           <Button label="8" onClick={() => handleAddNumber("8")} />
           <Button label="9" onClick={() => handleAddNumber("9")} />
-          <Button label="-" onClick={() => handleAddNumber("")} />
+          <Button label="-" onClick={handleRemNumbers} />
         </Rom>
         <Rom>
           <Button label="4" onClick={() => handleAddNumber("4")} />
@@ -57,12 +107,12 @@ function App() {
           <Button label="1" onClick={() => handleAddNumber("1")} />
           <Button label="2" onClick={() => handleAddNumber("2")} />
           <Button label="3" onClick={() => handleAddNumber("3")} />
-          <Button label="=" onClick={igual} />
+          <Button label="=" onClick={handleEquals} />
         </Rom>
 
       </Content>
     </Container>
-  );
+  )
 }
 
 export default App;
